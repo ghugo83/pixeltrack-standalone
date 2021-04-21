549c95dcf8fd68473bf64b3f863f95af026e5c7b
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2021-04-21 17:35:41 +0200

    Add PixelCPEFast, pixelCPEforGPU and PixelCPEFastESProducer

--- src/alpaka/bin/main.cc
+++ src/alpaka/bin/main.cc
@@ -141,7 +141,7 @@ int main(int argc, char** argv) {
 
           esmodules.emplace_back(prefix + "SiPixelFedCablingMapESProducer");
           esmodules.emplace_back(prefix + "SiPixelGainCalibrationForHLTESProducer");
-          /*esmodules.emplace_back(prefix + "PixelCPEFastESProducer");*/
+          esmodules.emplace_back(prefix + "PixelCPEFastESProducer");
         }
       };
       addModules("alpaka_serial_sync::", Backend::SERIAL);
