fc101b07476148129cf92cdc957c946878837b8d
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2021-04-28 14:46:38 +0200

    comments

--- src/alpaka/AlpakaDataFormats/PixelTrackAlpaka.h
+++ src/alpaka/AlpakaDataFormats/PixelTrackAlpaka.h
@@ -70,11 +70,10 @@ namespace pixelTrack {
 }  // namespace pixelTrack
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
-  //using PixelTrackHeterogeneous = HeterogeneousSoA<pixelTrack::TrackSoA>;
   using PixelTrackAlpaka = AlpakaDeviceBuf<pixelTrack::TrackSoA>;
   using PixelTrackHost = AlpakaHostBuf<pixelTrack::TrackSoA>;
 
-  // TO DO:ANOTHER OPTION IS TO CREATE A HeterogeneousSoA class,
+  // NB: ANOTHER OPTION IS TO CREATE A HeterogeneousSoA class,
   // with a AlpakaDeviceBuf<pixelTrack::TrackSoA> as a data member
   // and a toHostAsync function.
 
